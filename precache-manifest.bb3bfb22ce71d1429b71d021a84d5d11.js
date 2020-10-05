d613406c14627483aea956219001dc3dff657a9f
Serg <serggbobrov@gmail.com>
2020-10-05 15:10:35 +0300

    Updates

---
+++ precache-manifest.bb3bfb22ce71d1429b71d021a84d5d11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2785d072a492e527757f851c3a01bc6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "557c072901ab15693d4e",
+    "url": "/static/css/main.4da0c641.chunk.css"
+  },
+  {
+    "revision": "29d5371f0b5c57caf6f8",
+    "url": "/static/js/2.f1491b55.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f1491b55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "557c072901ab15693d4e",
+    "url": "/static/js/main.0a8f002c.chunk.js"
+  },
+  {
+    "revision": "b201489dcbcc9f77b2de",
+    "url": "/static/js/runtime-main.e19410a0.js"
+  }
+]);
